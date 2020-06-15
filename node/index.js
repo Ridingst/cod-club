1f4886bdb4b6530481b024fe72cfc117903bb6e9
Thomas Ridings <thomas.a.ridings@gmail.com>
2020-06-15 09:02:37 +0100

    fixing UTC time error...badly

--- node/index.js
+++ node/index.js
@@ -75,7 +75,7 @@ app.get('/last_updated', function(req, res) {
                         dt.getDate().toString().padStart(2, '0') }/${
                         (dt.getMonth()+1).toString().padStart(2, '0') }/${
                         dt.getFullYear().toString().padStart(4, '0') } ${
-                        dt.getHours().toString().padStart(2, '0') }:${
+                        (dt.getHours()+1).toString().padStart(2, '0') }:${
                         dt.getMinutes().toString().padStart(2, '0') }:${
                         dt.getSeconds().toString().padStart(2, '0')
                 }`

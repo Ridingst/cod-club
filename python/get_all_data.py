f3a26f9cf3ec659eed9c5d7447b77e1bc7f6868f
Thomas Ridings <thomas.ridings@intenthq.com>
2020-11-17 18:10:14 +0000

    Fixing fullStats bug

--- python/get_all_data.py
+++ python/get_all_data.py
@@ -23,7 +23,8 @@ async def getFriends(client):
 
 async def getPlayerStats(player):
     resp = dict()
-    resp['calcStats'] = await calcScores(resp['fullStats'])
+    fullStats = await player.profile(Title.ModernWarfare, Mode.Warzone)
+    resp['calcStats'] = await calcScores(resp[fullStats])
     return resp
 
 async def calcScores(stats):
